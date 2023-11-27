573e75b7bbb888f6e0b5033413918c038e7295c5
oatspear <oatspear@gmail.com>
2023-11-27 19:02:24 +0000

    bug fix

--- data/battle/effect_command_pointers.asm
+++ data/battle/effect_command_pointers.asm
@@ -102,6 +102,7 @@ BattleCommandPointers:
 	dw BattleCommand_HappinessPower
 	dw BattleCommand_Present
 	dw BattleCommand_DamageCalc
+	dw BattleCommand_CheckTurn         ; placeholder
 	dw BattleCommand_Safeguard
 	dw BattleCommand_CheckSafeguard
 	dw BattleCommand_GetMagnitude
