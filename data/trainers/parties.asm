03a29f0776aa2e5837c9d422d9516e96f744f40b
Oatspear <103125850+oatspear@users.noreply.github.com>
2023-12-08 11:02:30 +0000

    Update parties.asm

--- data/trainers/parties.asm
+++ data/trainers/parties.asm
@@ -2,15 +2,10 @@ INCLUDE "data/trainers/party_pointers.asm"
 
 Trainers:
 ; Trainer data structure:
-; - db "NAME@", TRAINERTYPE_* constant
+; - db "NAME@"
 ; - 1 to 6 Pokémon:
-;    * for TRAINERTYPE_NORMAL:     db level, species
-;    * for TRAINERTYPE_MOVES:      db level, species, 4 moves
-;    * for TRAINERTYPE_ITEM:       db level, species, item
-;    * for TRAINERTYPE_ITEM_MOVES: db level, species, item, 4 moves
+;    * db species, item, ability, EV bit field, 4 moves
 ; - db -1 ; end
-
-;   db species, item, EV bit field, 4 moves
 
 FalknerGroup:
 	; FALKNER (1)
