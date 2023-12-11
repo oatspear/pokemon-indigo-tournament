4ebf4bd41d7aa95e02280a4c42afa395341666bf
Oatspear <103125850+oatspear@users.noreply.github.com>
2023-12-11 16:59:05 +0000

    Update mold_breaker_suppressed_abilities.asm

--- data/abilities/mold_breaker_suppressed_abilities.asm
+++ data/abilities/mold_breaker_suppressed_abilities.asm
@@ -1,12 +1,20 @@
 MoldBreakerSuppressedAbilities:
 	db BATTLE_ARMOR
+	db FLASH_FIRE
+	db LIGHTNING_ROD
+	db LEVITATE
+	db SAP_SIPPER
+	; db VOLT_ABSORB
+	; db WATER_ABSORB
+	; db SAND_VEIL
+	; db THICK_FAT
+	; db STURDY
+	; db DRY_SKIN
 	; db BIG_PECKS
 	; db CLEAR_BODY
 	; db CONTRARY
 	; db DAMP
-	; db DRY_SKIN
 	; db FILTER
-	; db FLASH_FIRE
 	; db FUR_COAT
 	; db HYPER_CUTTER
 	; db IMMUNITY
@@ -14,9 +22,7 @@ MoldBreakerSuppressedAbilities:
 	; db INSOMNIA
 	; db KEEN_EYE
 	; db LEAF_GUARD
-	; db LEVITATE
 	; db LIGHT_METAL
-	; db LIGHTNING_ROD
 	; db LIMBER
 	; db MAGIC_BOUNCE
 	; db MAGMA_ARMOR
@@ -27,21 +33,15 @@ MoldBreakerSuppressedAbilities:
 	; db OVERCOAT
 	; db OWN_TEMPO
 	; db PASTEL_VEIL
-	; db SAND_VEIL
-	; db SAP_SIPPER
 	; db SHIELD_DUST
 	; db SNOW_CLOAK
 	; db SOLID_ROCK
 	; db SOUNDPROOF
 	; db STICKY_HOLD
-	; db STURDY
 	; db SUCTION_CUPS
 	; db TANGLED_FEET
-	; db THICK_FAT
 	; db UNAWARE
 	; db VITAL_SPIRIT
-	; db VOLT_ABSORB
-	; db WATER_ABSORB
 	; db WATER_VEIL
 	; db WHITE_SMOKE
 	; db WONDER_SKIN
